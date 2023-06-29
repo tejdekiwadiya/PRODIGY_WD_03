ff636b16a33486f96e8ffa496754e5029b400c26
Tej Dekiwadiya <105840549+tejdekiwadiya@users.noreply.github.com>
2023-06-29 20:33:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let clearbtn = document.getElementById("clear")
 let equal = document.getElementById("equal")
 let result = document.getElementById("result")
+let delet = document.getElementById("delete")
+let clearbtn_history = document.getElementById("button_c")
+let history = document.getElementById("button_h")
 
 //Clear Function
 function clear() {
-    let result = document.querySelector(".result")
     result.value = ''
 }
 clearbtn.addEventListener('click', clear)
@@ -20,4 +22,48 @@ function result_ans() {
         result_final = null
     }
 }
+
+//History Function
+function history_ans() {
+    let history = document.createElement("div")
+    history.setAttribute('id', 'history_list')
+    history.innerHTML = result.value.toString()
+    document.getElementById("history").appendChild(history)
+
+}
+equal.addEventListener('click', history_ans)
 equal.addEventListener('click', result_ans)
+
+//Delete Function
+function dele() {
+    let len_array = Array.from(result.value)
+    let array_pop = len_array.pop()
+    len_array = len_array.toString()
+    for (let index = 0; index <= len_array.length; index++) {
+        len_array = len_array.replace(',', '')
+    }
+    result.value = len_array
+}
+delet.addEventListener('click', dele)
+
+
+//History Clear
+function history_clear() {
+    while (document.getElementById("history").firstChild) {
+        document.getElementById("history").removeChild(document.getElementById("history").firstChild);
+    }
+}
+clearbtn_history.addEventListener('click', history_clear)
+
+
+//Histoy Show/Hide
+function S_H() {
+    let toggle = document.querySelector(".history")
+    if (toggle.style.display === 'block') {
+        toggle.style.display = 'none'
+    }
+    else {
+        toggle.style.display = 'block'
+    }
+}
+history.addEventListener('click', S_H)
